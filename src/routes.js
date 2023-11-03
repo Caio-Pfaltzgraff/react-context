3d757e3b2cd9f662a63fdef6aac901919742a78a
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-03 12:11:06 -0300

    Usando o Context nas telas e o useContext

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,16 @@ import Feira from "pages/Feira";
 import Login from "pages/Login";
 import Carrinho from "pages/Carrinho";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import { UsuarioContext } from "common/context/Usuario";
+import { UsuarioProvider } from "common/context/Usuario";
 
 function Router() {
-  const [nome, setNome] = useState("")
-  const [saldo, setSaldo] = useState(0)
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <UsuarioContext.Provider value={{nome, setNome, saldo, setSaldo}}>
+          <UsuarioProvider>
             <Login/>
-          </UsuarioContext.Provider> 
+          </UsuarioProvider> 
         }/>
         <Route path="feira" element={<Feira />} />
         <Route path="carrinho" element={<Carrinho />} />
