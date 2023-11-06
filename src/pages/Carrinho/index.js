6e1e7e90ac4b9accbcf69f9cd534c9df8a13ab80
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-06 16:09:56 -0300

    Criando o contexto de pagamento

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -6,15 +6,20 @@ import {
 } from "./styles";
 import { Button, Snackbar, InputLabel, Alert } from "@mui/material";
 import { useCarrinhoContext } from "common/context/Carrinho";
+import { PagamentoContext } from "common/context/Pagamento";
 import Produto from "components/Produto";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Carrinho() {
   const [openSnackBar, setOpenSnackBar] = useState(false);
   const { carrinho } = useCarrinhoContext();
+  const { formaPagamento } = useContext(PagamentoContext)
+  const navigate = useNavigate();
   return (
     <Container>
-      <Voltar />
+      <Voltar onClick={() => navigate(-1)}/>
+      {formaPagamento.nome}
       <h2>Carrinho</h2>
       {carrinho.map((produto) => (
         <Produto {...produto} key={produto.id}/>
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Carrinho from "pages/Carrinho";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UsuarioProvider } from "common/context/Usuario";
 import { CarrinhoProvider } from "common/context/Carrinho";
+import { PagamentoProvider } from "common/context/Pagamento";
 
 function Router() {
   return (
@@ -24,7 +25,9 @@ function Router() {
         <Route path="/carrinho" element={
           <UsuarioProvider>
             <CarrinhoProvider>
+              <PagamentoProvider>
                 <Carrinho />
+              </PagamentoProvider>
             </CarrinhoProvider>
           </UsuarioProvider>
         }/>
