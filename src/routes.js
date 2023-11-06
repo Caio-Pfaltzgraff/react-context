ecbd03d5042902235acbc86603930a5808f515df
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-06 11:43:42 -0300

    Criando o contexto de Carrinho e add produto

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,29 @@ import Login from "pages/Login";
 import Carrinho from "pages/Carrinho";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UsuarioProvider } from "common/context/Usuario";
+import { CarrinhoProvider } from "common/context/Carrinho";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <UsuarioProvider>
-            <Login/>
-          </UsuarioProvider> 
+            <UsuarioProvider>
+                <Login />
+            </UsuarioProvider>
         }/>
-        <Route path="feira" element={<Feira />} />
-        <Route path="carrinho" element={<Carrinho />} />
+        <Route path="/feira" element={
+            <UsuarioProvider>
+                <CarrinhoProvider>
+                    <Feira />
+                </CarrinhoProvider>
+            </UsuarioProvider>
+        }/>
+        <Route path="/carrinho" element={
+            <CarrinhoProvider>
+                <Carrinho />
+            </CarrinhoProvider>
+        }/>
       </Routes>
     </BrowserRouter>
   );
